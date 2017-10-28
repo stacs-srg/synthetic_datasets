91bb94e2b468f50edf2beae80799662fc04eae42
sic2 <sic2@st-andrews.ac.uk>
2017-10-28 18:35:03 +0100

    synthetic fs dataset class

---
+++ src/main/java/uk/ac/standrews/cs/synthetic_datasets/file_system/SyntheticFSDataset.java
@@ -0,0 +1,17 @@
+package uk.ac.standrews.cs.synthetic_datasets.file_system;
+
+/**
+ * Generate a Synthetic FS from some FS_stats
+ * @author Simone I. Conte "sic2@st-andrews.ac.uk"
+ */
+public class SyntheticFSDataset {
+
+    /**
+     *
+     * @param fs_stats reference stats to create new dataset
+     * @param source this is where data is taken to create the synthetic dataset
+     */
+    public SyntheticFSDataset(FS_stats fs_stats, Object source) {
+
+    }
+}
